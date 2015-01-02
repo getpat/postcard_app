a949d545d2fda28466a3df110d501defe366ddbe
pat <getpat@gmail.com>
2015-01-02 11:46:34 -0600

    second rev comment to test commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // Add a comment to test the commit function in github
     }
 
     
